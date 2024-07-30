4ed9c605cd8649ea518b1c2108741b7d75c2e2fd
iyugov <iyugov@gmail.com>
2024-07-30 14:14:30 +0300

    Make separate settings for development.

--- education/education/settings/base.py
+++ education/education/settings/base.py
@@ -14,20 +14,11 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j__^r-dso#ok_*5+%j(a@u66_^ca@p&a04!1x*mqeauwvzvyi#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,18 +63,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'education.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
---
+++ education/education/settings/development.py
@@ -0,0 +1,14 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-j__^r-dso#ok_*5+%j(a@u66_^ca@p&a04!1x*mqeauwvzvyi#'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db-development.sqlite3',
+    }
+}
